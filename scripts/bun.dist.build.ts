f91f8c784f568a08b2f0cee66f65582f731a62a3
citkane <michael@openpoint.ie>
2024-12-07 19:08:29 +0000

    Include deno install

---
+++ scripts/bun.dist.build.ts
@@ -0,0 +1,32 @@
+import { $ } from "bun";
+import pkg from "../package.json";
+
+await $`
+    rm -rf ./dist/**/*.*
+    bun build --target=node --sourcemap=external ./src --outdir ./dist
+    bun build --target=node --sourcemap=external ./src/install --outdir ./dist/src
+    cp -R ./cmake ./dist
+    # cp -R ./docs ./dist
+    cp ./deno.json ./dist
+    cp ./LICENSE ./dist
+    cp ./README.md ./dist
+`;
+
+const nodeBunInstall =
+      '%PATH% && (echo %PATH% | findstr /C:"node-gyp-bin:" >nul && node src/install/index.js || bun src/install/index.js) || echo $PATH | grep -q "node-gyp-bin:" && node src/install/index.js || bun src/install/index.js';
+
+const package_json = Object.keys(pkg).reduce((distPkg, key) => {
+      const k = key as keyof typeof pkg;
+      switch (key) {
+            case "scripts":
+                  distPkg.scripts = { install: nodeBunInstall };
+                  break;
+            case "devDependencies":
+                  break;
+            default:
+                  distPkg[key] = pkg[k];
+      }
+      return distPkg;
+}, {} as Record<string, any>);
+
+await Bun.write("./dist/package.json", JSON.stringify(package_json, null, "\t"));
